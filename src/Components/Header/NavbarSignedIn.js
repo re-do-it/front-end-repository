d35bc4257a02be0a3a3835e8224afdfae8058dae
sanand100 <shraddhaanand1@gmail.com>
2022-07-11 18:00:23 -0400

    Add navbar signed in and out components + styling.
    Co-authored-by: Julian Segura <juliansrecent@gmail.com>
    Co-authored-by: Adrian Guia <adriang1702@gmail.com>
    Co-authored-by: Isaac Kim <kimisaac1998@gmail.com>"

--- src/Components/Header/NavbarSignedIn.js
+++ src/Components/Header/NavbarSignedIn.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { useNavigate } from 'react-router-dom';
 
-const NavbarSignedIn = () => {
-    return (
-        <div>
-            hello from navbar signed in
-        </div>
-    );
-};
+function NavbarSignedIn(props) {
+	let navigate = useNavigate();
+
+	// function for the logo button to redirect to the home
+	function logoButton(event) {
+		event.preventDefault();
+		navigate('/');
+	}
+
+	// function that will work with the search
+	function handleSubmit() {
+		console.log('click from handleSubmit');
+	}
+
+	return (
+		<div className='navbarSO'>
+			<Navbar className='form'>
+				<Container>
+					<Navbar.Brand>
+						<img src='asdfasdf' alt='my image' onClick={logoButton} />
+					</Navbar.Brand>
+					<Form>
+						<Form.Control
+							className='input'
+							type='text'
+							placeholder='Search Redo-it'
+						/>
+					</Form>
+					<Nav className='me-auto'>
+						<Nav.Link>
+							<Dropdown>
+								<Dropdown.Toggle className='btn'>User</Dropdown.Toggle>
+
+								<Dropdown.Menu>
+									<Dropdown.Item href='#/signout'>Sign out</Dropdown.Item>
+									<Dropdown.Item href='#/profile'>My profile</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
+						</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+		</div>
+	);
+}
 
 export default NavbarSignedIn;
