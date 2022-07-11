119ae9ec036d98be4322d00b1cfcc80979c221eb
JuliansRecent <juliansrecent@gmail.com>
2022-07-11 11:29:14 -0700

    Make styling changes, set default styling properties.

--- src/Components/Main/SignupPage.js
+++ src/Components/Main/SignupPage.js
@@ -32,11 +32,9 @@ function SignupPage(props) {
 			<Form
 				onSubmit={handleSubmit}
 				id='sign-in-form'
-				className='text-center w-100'
+				className='text-center w-100 form'
 				style={{
-					padding: '1em 3em',
-					boxShadow: '0 4px 15px 0',
-					backgroundColor: 'rgb(250,250,255)',
+					padding: '1em 3em'
 				}}>
 				<h1 className='mb-5 fs-3 fw-normal'>Please Sign up</h1>
 				<Form.Group controlId='name'>
@@ -45,7 +43,7 @@ function SignupPage(props) {
 						size='lg'
 						placeholder='Your Name'
 						// autoComplete='username'
-						className='position-relative mb-1'
+						className='position-relative mb-1 input'
 						value={formState.name}
 						onChange={handleChange}
 					/>
@@ -56,7 +54,7 @@ function SignupPage(props) {
 						size='lg'
 						placeholder='Email address'
 						autoComplete='username'
-						className='position-relative mb-1'
+						className='position-relative mb-1 input'
 						value={formState.username}
 						onChange={handleChange}
 					/>
@@ -67,7 +65,7 @@ function SignupPage(props) {
 						size='lg'
 						placeholder='Password'
 						autoComplete='current-password'
-						className='position-relative mb-4'
+						className='position-relative mb-4 input input-hover'
 						value={formState.password}
 						onChange={handleChange}
 					/>
@@ -75,17 +73,17 @@ function SignupPage(props) {
 
 				<div className='button-box'>
 					<div className='d-grid mb-5 signup button '>
-						<Button variant='secondary' type='button'>
+						<button className='btn' type='button'>
 							<p className='signup-text'>Already have an account?</p>
 							<a href='' className='signup-text'>
 								Log in!
 							</a>
-						</Button>
+						</button>
 					</div>
 					<div className='d-grid mb-5 signin button'>
-						<Button variant='primary' type='submit'>
+						<button className='btn' type='submit'>
 							Sign In
-						</Button>
+						</button>
 					</div>
 				</div>
 
