317577b23d98ce41a85850e0b6c77e787e25a8fb
blees12345 <adriang1702@gmail.com>
2022-07-13 09:19:40 -0700

    added create post functionality

--- src/Components/Main/Homepage/CreatePost.js
+++ src/Components/Main/Homepage/CreatePost.js
@@ -1,16 +1,25 @@
 import './CreatePost.css';
-
+import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 
 function CreatePost(props) {
+      const [searchString, setSearchString] = useState('');
+
+    function handleChange(event) {
+			setSearchString(event.target.value);
+           console.log('text')
+		} 
+        function handleSubmit(event) {
+		event.preventDefault();
+        }
 	return (
-		<Form className='create-post d-flex flex-column align-items-center p-3 gap-3'>
-			<Form.Control className='title-input' type='text' placeholder='Title' />
+		<Form className='create-post d-flex flex-column align-items-center p-3 gap-3' onSubmit={handleSubmit}>
+			<Form.Control className='title-input' type='text' placeholder='Title' onChange={handleChange} />
 
 			<Form.Control
 				className='body-input d-flex justify-content-start'
 				type='text'
-				placeholder='Your Text'
+				placeholder='Your Text' onChange={handleChange}
 			/>
 
 			<div className='bottom-button-container d-flex justify-content-end gap-2'>
@@ -18,6 +27,8 @@ function CreatePost(props) {
 			</div>
 		</Form>
 	);
-}
+    }
+
+
 
 export default CreatePost;
