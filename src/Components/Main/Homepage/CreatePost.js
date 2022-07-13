00445b741497e9741e3c4dc0817861445996f419
blees12345 <adriang1702@gmail.com>
2022-07-13 11:43:59 -0700

    linked creat post to api
    Co-authored-by: Julian Segura <juliansrecent@gmail.com>
    Co-authored-by: Adrian Guia <adriang1702@gmail.com>
    Co-authored-by: Isaac Kim <kimisaac1998@gmail.com>
    Co-authored-by: Shraddha Anand <shraddhaanand1@gmail.com>

--- src/Components/Main/Homepage/CreatePost.js
+++ src/Components/Main/Homepage/CreatePost.js
@@ -5,22 +5,24 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function CreatePost(props) {
-    const navigate = useNavigate();
-      const [searchString, setSearchString] = useState({
-				title: '',
-				body: '',
-			});
+   const navigate = useNavigate();
+		const initialPostState = {
+			title: '',
+			body: '',
+			
+		};
 
-    function handleChange(event) {
-			setSearchString(event.target.value);
-          
-		} 
+		const [postState, setPostState] = useState(initialPostState);
+
+   function handleChange(event) {
+			setPostState({ ...postState, [event.target.id]: event.target.value });
+		}
         const handleSubmit =async (event) => {
 		event.preventDefault();
         try {
 					const response = await axios.post(
-						'https://redoit-api.herokuapp.com/api/post',
-						searchString
+						'http://localhost:8000/api/posts',
+						postState
 					);
 					// const response = await fetch('http://localhost:3111/icecreams', {
 					// 	method: 'POST',
@@ -31,24 +33,37 @@ function CreatePost(props) {
 					// });
 					console.log(response);
 					if (response.status === 201) {
+						navigate('/');
 						console.log(response)
 					}
 				} catch (error) {
 					console.log(error);
 				}
-				
+			
         }
 	return (
+		<Form
+			className='create-post d-flex flex-column align-items-center p-3 gap-3 mt-5'
+			onSubmit={handleSubmit}>
+			<Form.Group controlId='title'>
+				<Form.Control
+					className='title-input'
+					type='text'
+					placeholder='Title'
+					value={postState.title}
+					onChange={handleChange}
+				/>
+			</Form.Group>
 
-		<Form className='create-post d-flex flex-column align-items-center p-3 gap-3 mt-5' onSubmit={handleSubmit}>
-			<Form.Control className='title-input' type='text' placeholder='Title' onChange={handleChange}/>
-
-
-			<Form.Control
-				className='body-input d-flex justify-content-start'
-				type='text'
-				placeholder='Your Text' onChange={handleChange}
-			/>
+			<Form.Group controlId='body'>
+				<Form.Control
+					className='body-input d-flex justify-content-start'
+					type='text'
+					placeholder='Your Text'
+					onChange={handleChange}
+					value={postState.body}
+				/>
+			</Form.Group>
 
 			<div className='bottom-button-container d-flex justify-content-end gap-2'>
 				<button className='btn'>Cancel</button>
