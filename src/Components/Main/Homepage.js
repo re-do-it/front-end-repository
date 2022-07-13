2c63046c350d09f9d256d71e42e9c7ba1be7a0c8
sanand100 <shraddhaanand1@gmail.com>
2022-07-13 15:31:13 -0400

    Add delete posts route with heroku.
    Co-authored-by: Julian Segura <juliansrecent@gmail.com>
    Co-authored-by: Adrian Guia <adriang1702@gmail.com>
    Co-authored-by: Isaac Kim <kimisaac1998@gmail.com>
    Co-authored-by: Shraddha Anand <shraddhaanand1@gmail.com>

--- src/Components/Main/Homepage.js
+++ src/Components/Main/Homepage.js
@@ -1,13 +1,45 @@
 import FilterPosts from './Homepage/FilterPosts';
 import { useState } from 'react';
 import ContentCards from './Homepage/ContentCards';
-import './Homepage.css'
+import './Homepage.css';
+// import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { faPooStorm } from '@fortawesome/free-solid-svg-icons';
 
 function Homepage(props) {
+	const [currentPosts, setCurrentPosts] = useState([]);
+	//fetch all the posts
+	const getPosts = async () => {
+		try {
+			const res = await axios.get(`https://redoit-api.herokuapp.com/api/posts`);
+			let data = res.data;
+			console.log(data);
+			setCurrentPosts(data.reverse());
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getPosts();
+	}, []);
+
 	return (
-		<div className="mt-2 d-flex flex-column gap-2">
+		<div className='mt-2 d-flex flex-column gap-2'>
 			<FilterPosts />
-			<ContentCards />
+			{/* map through the fetched data and display them using the card */}
+			{currentPosts.map((post) => {
+				return (
+					<ContentCards
+						title={post.title}
+						body={post.body}
+						createdAt={post.createdAt}
+						id={post._id}
+						getPosts={getPosts}
+					/>
+				);
+			})}
 		</div>
 	);
 }
